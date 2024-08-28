fbf82fca496a170306ea3b362ccceeb8e453eb97
Sameer <sameer.uzzamans@gmail.com>
2024-08-28 15:03:45 +0500

    Removed fontawsome

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule,FontAwesomeModule],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
